8991497201098a01053f7a8ca018bac91668d956
Michal <m.horodelski@medidok.de>
2022-10-28 07:34:57 +0200

    Add project files.

---
+++ AsocjacjeWykladNr03.Runner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsocjacjeWykladNr03.Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
